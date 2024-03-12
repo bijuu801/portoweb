5b99479785e18ad8a60d08765d08ea3bd0885c3a
ALWSU-dotcom <alexanderolson2@mail.weber.edu>
2024-03-11 18:02:32 -0600

    added more music

--- src/components/SoundCloudEmbed.jsx
+++ src/components/SoundCloudEmbed.jsx
@@ -1,18 +1,17 @@
-const SoundCloudEmbed = () => {
+const SoundCloudEmbed = ({ song_link, song_title, track_id }) => {
+    const embedUrl = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${track_id}&color=%23287617&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`
+
     return (
         <div>
             <iframe
                 width="95%"
                 height="180"
-                scrolling="no"
-                frameBorder="no"
                 allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1173664141&color=%23287617&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+                src={embedUrl}>
             </iframe>
             <div style={{ fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda, Verdana, Tahoma, sans-serif', fontWeight: '100' }}>
-                <a href="https://soundcloud.com/liveatthebijuu" title="bijuu" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>bijuu</a>
-                ·
-                <a href="https://soundcloud.com/liveatthebijuu/suffer-machine" title="suffer machine" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>suffer machine</a>
+                <a href="https://soundcloud.com/liveatthebijuu" title="bijuu" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }} />
+                <a href={song_link} title={song_title} target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }} />
             </div>
         </div>
     );
--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -8,13 +8,16 @@ const MusicPage = () => {
       centerColumnContent={
         <div className="container mx-auto px-6 mt-8">
           <div className="justify-center flex-wrap ml-8">
-            <SoundCloudEmbed />
+            <SoundCloudEmbed song_link="https://soundcloud.com/liveatthebijuu/pique-thru-the-blinds" song_title="pique thru the blinds" track_id="1539925177"/>
           </div>
           <div className="justify-center flex-wrap my-4 ml-8">
-            <SoundCloudEmbed />
+            <SoundCloudEmbed song_link="https://soundcloud.com/liveatthebijuu/ill-never-b-picture-perfect" song_title="ill never b picture perfect beyonce" track_id="1529074732"/>
           </div>
           <div className="justify-center flex-wrap my-4 ml-8">
-            <SoundCloudEmbed />
+            <SoundCloudEmbed song_link="https://soundcloud.com/liveatthebijuu/if-only-4-one-night" song_title="if only 4 one night" track_id="871949812"/>
+          </div>
+          <div className="justify-center flex-wrap my-4 ml-8">
+            <SoundCloudEmbed song_link="https://soundcloud.com/liveatthebijuu/no-s" song_title="no ?s" track_id="1539925180" />
           </div>
         </div>
       }
@@ -24,3 +27,4 @@ const MusicPage = () => {
 };
 
 export default MusicPage;
+
